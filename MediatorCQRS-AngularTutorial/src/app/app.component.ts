82a233670d96f2faa0431d752d63a53b65114eb6
Constantin Bivol <constantin.bivol@amdaris.com>
2020-12-29 12:53:01 +0200

    Added some changes

--- MediatorCQRS-AngularTutorial/src/app/app.component.ts
+++ MediatorCQRS-AngularTutorial/src/app/app.component.ts
@@ -1,37 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Initial';
-  number = 4;
-  arr = [1, 2, 3, 4];
-  obj = { a: 1, b: { c: 2 } };
-
-  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png';
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Time is over')
-    // tslint:disable-next-line: max-line-length
-    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png';
-    // }, 5000)
-  }
-  inputValue = '';
-
-  // onInput(event: KeyboardEvent): void {
-  //   console.log('Event', event);
-  //   this.inputValue = (event.target as HTMLInputElement).value;
-  // }
-  onInput(event: any): void {
-    this.title = event.target.value;
-  }
-  onClick(): void {
-    console.log('Click');
-  }
-  onBlur(str: string): void {
-    this.inputValue = str;
+  posts: Post[] = [
+    { title: 'Vreau sa invat Angular', text: 'eu inca invat', id: 1 },
+    { title: 'Next block', text: 'directive', id: 2 }
+  ];
+  updatePosts(post: Post): void {
+    this.posts.unshift(post);
   }
 }
--- MediatorCQRS-AngularTutorial/src/app/app.module.ts
+++ MediatorCQRS-AngularTutorial/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
 import { FormsModule } from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
