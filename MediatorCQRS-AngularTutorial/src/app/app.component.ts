fceb7b0fc000518ef87d9af0a2ea7bf466f97b34
Constantin Bivol <constantin.bivol@amdaris.com>
2020-12-29 09:27:26 +0200

    added some components

--- MediatorCQRS-AngularTutorial/src/app/app.component.ts
+++ MediatorCQRS-AngularTutorial/src/app/app.component.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MediatorCQRS-AngularTutorial';
-  number = 4 ;
-  arr  = [1,2,3,4];
+  title = 'Initial';
+  number = 4;
+  arr = [1, 2, 3, 4];
+  obj = { a: 1, b: { c: 2 } };
+
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png';
+  constructor() {
+    // setTimeout(() => {
+    //   console.log('Time is over')
+    // tslint:disable-next-line: max-line-length
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png';
+    // }, 5000)
+  }
+  inputValue = '';
+
+  // onInput(event: KeyboardEvent): void {
+  //   console.log('Event', event);
+  //   this.inputValue = (event.target as HTMLInputElement).value;
+  // }
+  onInput(event: any): void {
+    this.title = event.target.value;
+  }
+  onClick(): void {
+    console.log('Click');
+  }
+  onBlur(str: string): void {
+    this.inputValue = str;
+  }
 }
--- MediatorCQRS-AngularTutorial/src/app/app.module.ts
+++ MediatorCQRS-AngularTutorial/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { PostComponent } from './post/post.component';
     PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
